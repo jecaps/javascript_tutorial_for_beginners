3fc9995096892a213cd6bcd30ef2104a5b7c61f1
Jerika Capila <jerikacapila@gmail.com>
2021-08-21 10:15:54 +0200

    Ptimitive Data Types

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-// Constants
+let name = "John"; //String Literal
+let age = 30; // Number Literal
+let lovesCats = true; //Boolean Literal
+let color = null; // Null
+let list; //Undefined
 
-// Cannot be reassigned
+name += " Smith"; // Adding two strings
+console.log(name);
 
-const pi = 3.14;
+// let message = name + " is " + age;
+let message = `${name} is ${age}`; // use back ticks to use both '' "" in a string
+console.log(message);
 
-console.log(pi)
+age /= 3;
+console.log(age);
