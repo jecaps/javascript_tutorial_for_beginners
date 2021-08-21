0731dcfa01a0a045087bb2f936b5abbc3c61b781
Jerika Capila <jerikacapila@gmail.com>
2021-08-21 09:41:46 +0200

    Constants

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
-// Variables
+// Constants
 
-// Are Case-Sensitive
-// Should be meaningful
-// Cannot start with a number
-// Cannot contain a space or a hyphen
-// Cannot be a reserved word
+// Cannot be reassigned
 
-let age;
-age = 30
-age = 35
+const pi = 3.14;
 
-let a, b, c;
-a = 1;
-b = 2;
-c = 3;
-
-console.log(a*2);
+console.log(pi)
